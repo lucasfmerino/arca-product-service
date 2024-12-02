dee5332a382c540a40328ed116a67c97470205c6
lucasfmerino <lucasfmerino@gmail.com>
2024-12-02 10:52:08 -0300

    feat(infra): added infra documentation

---
+++ src/main/java/com/arca/product_service/infra/documentation/WebSupport.java
@@ -0,0 +1,10 @@
+package com.arca.product_service.infra.documentation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebSupport
+{
+}
